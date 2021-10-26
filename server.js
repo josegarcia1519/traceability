e0c7fa88abe51740157bb1e22befe443e8f6ef0c
josegarcia1519 <josegarcia1519@gmail.com>
2021-10-26 15:51:57 -0600

    update6

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var rollbar = new Rollbar({
 //unicycle();
 
 app.get('/', function(req, res){
- res.sendFile(path.join(__dirname, "///index.html"))
+ res.sendFile(path.join(__dirname, "./public/index.html"))
  rollbar.log("Hello world!");
 })
 
